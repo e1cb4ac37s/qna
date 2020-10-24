faa9522114bea0d0789f10773445c218c8e1a2f2
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-10-24 08:55:29 +0300

    3_tdd: answer added, validations added

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
+  it { should validate_length_of(:title).is_at_most(255) }
   it { should validate_presence_of :body }
+  it { should have_many(:answers) }
 end
